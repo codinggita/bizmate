2c61131867ace2022341a9e3b9c6926c38e9dc4e
prokrishpatel <pro.krishpatel@gmail.com>
2024-01-19 17:24:54 +0530

    Created Welcome Page

---
+++ src/client/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import './style/App.css';
+
+function App() {
+  const [showLanding, setShowLanding] = useState(true);
+
+  useEffect(() => {
+    // Set a timer to hide the Landing page after 5 seconds
+    const timer = setTimeout(() => {
+      setShowLanding(false);
+    }, 3000);
+
+    // Clear the timer when the component unmounts to avoid memory leaks
+    return () => clearTimeout(timer);
+  }, []); // empty dependency array ensures that this effect runs only once on mount
+
+  return (
+    <div className="main-frame">
+      {showLanding && <Landing />}
+      {!showLanding && <Dashboard />}
+    </div>
+  );
+}
+
+export default App;
