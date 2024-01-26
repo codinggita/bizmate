ef232bf06c73851a507db7d4f398f64331322510
prokrishpatel <pro.krishpatel@gmail.com>
2024-01-26 12:24:52 +0530

    updated landing

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,22 +4,9 @@ import Home from "./Home";
 import './style/App.css';
 
 function App() {
-  const [showLanding, setShowLanding] = useState(true);
-
-  useEffect(() => {
-    // Set a timer to hide the Landing page after 5 seconds
-    const timer = setTimeout(() => {
-      setShowLanding(false);
-    }, 3000);
-
-    // Clear the timer when the component unmounts to avoid memory leaks
-    return () => clearTimeout(timer);
-  }, []); // empty dependency array ensures that this effect runs only once on mount
-
   return (
     <div className="main-frame">
-      {showLanding && <Landing />}
-      {!showLanding && <Home />}
+      <Landing />
     </div>
   );
 }
--- src/client/Landing.jsx
+++ src/client/Landing.jsx
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import bizmate from './assets/bizmate.png';
+import bizmateLogo from './assets/logoNobg.png';
 import './style/landing.css';
 
 const Landing = () => {
-  const [addSlideOutDown, setAddSlideOutDown] = useState(false);
-
-  useEffect(() => {
-    // Set a timeout to add the second class after 2.5 seconds
-    const timeoutId = setTimeout(() => {
-      setAddSlideOutDown(true);
-    }, 2300);
-
-    // Cleanup the timeout on component unmount
-    return () => clearTimeout(timeoutId);
-  }, []);
-
-  // Concatenate the classes based on the state
-  const imageClasses = `animate__animated animate__zoomInDown landing-logo ${addSlideOutDown ? 'animate__slideOutDown' : ''}`;
-
   return (
     <div className="landing">
-      <img src={bizmate} className={imageClasses} alt="Bizmate Logo" />
+      <div className="blurry-header-helper"></div>
+      <div className="header">
+        <div className="head-left">
+          <div className="logo-head">
+              <img src={bizmateLogo} alt="Bizmate Logo" />
+              <span>BizMate</span>
+          </div>
+        </div>
+        <div className="head-right">
+
+        </div>
+      </div>
+      <div className="extra"></div>
+      <div className="land-main">
+        <div className="circle-color">
+          <img src={bizmate}/>
+        </div>
+      </div>
     </div>
   );
 }
