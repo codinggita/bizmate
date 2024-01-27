2a7be681e1c8e673e95cc68fa98979da12b99eb0
prokrishpatel <pro.krishpatel@gmail.com>
2024-01-28 02:46:37 +0530

    bug resolved

---
+++ src/client/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './style/dashboard.css'
+
+const Dashboard = ()=>{
+    return(
+        <div className="dashboard">
+            Dashboard
+        </div>
+    );
+}
+export default Dashboard;
