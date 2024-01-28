1dfce3dfc1aa86b6663dc7fbf77f0e91543d617c
prokrishpatel <pro.krishpatel@gmail.com>
2024-01-28 12:20:30 +0530

    Added Home Page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Landing from "./Landing";
+import Home from "./Home";
 import './style/App.css';
 
 function App() {
   return (
     <div className="main-frame">
-      <Landing />
+      {/* <Landing /> */}
+      <Home />
     </div>
   );
 }
